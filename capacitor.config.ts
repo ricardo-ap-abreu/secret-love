a0ff8841031d62cb26cc57ea442e0f4e699215ac
Ricardo Abreu <ricardo.abreu_ext@interplayers.com.br>
2022-05-20 23:44:54 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'secret love sex shop',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
